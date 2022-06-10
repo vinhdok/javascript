e3e22fb415b97823e4f40ee26336b5c8cc98df4e
Vinh Dok <47440740+vinhdok@users.noreply.github.com>
2022-06-10 07:57:46 -0400

    Add files via upload

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -190,7 +190,7 @@ console.log(age1, age2, age3);
 
 const ages = [age1, age2, age3];
 console.log(ages);
-*/
+
 
 //litteral syntax
 const friends = ['Christian', 'Kheng', 'Vinh', 'Peter'];
@@ -227,3 +227,143 @@ if (friends.includes('Peter')) {
 } else {
     console.log('you dont have a friend named Peter');
 }
+
+//tips function
+const tipAmount = function calcTip(bill) {
+    if (bill > 50 && bill < 300) {
+        //console.log(bill);
+        console.log("15%");
+        return (bill * .15);
+    } else {
+        // console.log(bill);
+        console.log("20%");
+        return (bill * .2);
+
+    }
+}
+//const billAmount = 100;
+const billsArray = [125, 555, 44];
+const tipsArray = [tipAmount(billsArray[0]), tipAmount(billsArray[1]), tipAmount(billsArray[2])];
+
+console.log(`the tip amounts is ${tipsArray[0]} for a bill of ${billsArray[0]} -  tip amount is ${tipsArray[1]} for a bill of ${billsArray[1]} -  tip amount is ${tipsArray[2]} for a bill of ${billsArray[2]}`);
+
+
+console.log(`Total 1: ${billsArray[0] + tipsArray[0]}, Total 2: ${billsArray[1] + tipsArray[1]}, Total 3: ${billsArray[2] + tipsArray[2]}`);
+
+
+console.log(billsArray, tipsArray);
+
+
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Dok',
+    age: 2037 - 1991,
+    job: 'tecacher',
+    friends: ['Mike', 'Peter', 'Steven']
+};
+
+//dot notation - need to use the real property name
+console.log(jonas);
+
+console.log(jonas.lastName);
+
+//bracket notation we can build a string to search the object
+console.log(jonas['lastName']);
+
+const nameKey = 'Name';
+console.log(jonas['first' + nameKey]);
+console.log(jonas['last' + nameKey]);
+
+/*
+const interestedIn = prompt('What do you want to know about Jonas?  Choose between first name, last name, age, job, and friends');
+console.log(interestedIn);
+
+console.log(jonas[interestedIn]);
+
+if (jonas[interestedIn]) {
+    console.log(jonas[interestedIn]);
+} else {
+    console.log('No value defined')
+}
+
+jonas.location = 'Portugal';
+jonas['twitter'] = '@jonassschedtman';
+console.log(jonas);
+
+
+
+//challenge
+//Jonas has 3 friends, and his best friend s called Michael
+const friendCount = jonas.friends;
+const friendCount2 = (friendCount.length);
+console.log(`${jonas['firstName']} has ${friendCount2}, and his best friend is called ${jonas.friends[0]}`);
+
+*/
+
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Dok',
+    birthYear: 1991,
+    job: 'tecacher',
+    friends: ['Mike', 'Peter', 'Steven'],
+    hasDriversLicense: true,
+
+    // calcAge: function (birthYear) {
+    //     //console.log(this);
+    //     return 2037 - birthYear;
+    // }
+
+    // calcAge: function (birthYear) {
+    //     //console.log(this);
+    //     return 2037 - this.birthYear;
+    // }
+
+    // calcAge: function () {
+    //     // console.log(this);
+    //     return 2037 - this.birthYear;
+    // }
+
+    calcAge: function () {
+        this.age = 2037 - this.birthYear;
+        return this.age;
+    },
+    //ternary operator
+    getSummary: function () {
+        return (`${this.firstName} is a ${this.calcAge()} years old ${this.job}, and he as ${this.hasDriversLicense ? 'a' : 'no'} drivers license`);
+    }
+
+};
+//console.log(jonas.calcAge());
+
+// console.log(jonas.age);
+// console.log(jonas.age);
+// console.log(jonas.age);
+// console.log(jonas.age);
+//console.log(jonas['calcAge'](jonas.birthYear));
+
+//Challlenge
+//"Jonas is a 46-year old teacher, and he has a/no drivers license"
+
+// const calcAge = function (birthYear) {
+//     return 2037 - birthYear;
+
+// }
+
+console.log(jonas.hasDriversLicense);
+
+function canDrive() {
+    if (jonas.hasDriversLicense) {
+        return 'a';
+    } else {
+        return 'no';
+    }
+}
+
+const driver = canDrive();
+console.log(driver);
+
+
+console.log(`${jonas.firstName} is a ${jonas.calcAge()} years old ${jonas.job}, and he has ${driver} drivers license`);
+
+//console.log(jonas.getSummary());
+
